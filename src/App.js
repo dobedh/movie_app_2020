8d0c3868e885603c6c2b6d122d8e5153c07675d3
김동현 <dobedh@gmail.com>
2020-07-22 21:59:56 +0900

    200722_react_study

--- src/App.js
+++ src/App.js
@@ -1,11 +1,38 @@
 import React from 'react';
 
+function Food({ name, picture }){
+return ( 
+  <div>
+    <h2>I like { name }</h2>
+    <img src={picture} />
+  </div>
+)
+  }
+
+const foodILike = [
+  {
+    name : 'pizza',
+    image : 'https://www.recipetineats.com/wp-content/uploads/2020/05/Pepperoni-Pizza_7.jpg'
+  },
+  {
+    name : 'samgyeopsal',
+    image : 'https://image.chosun.com/sitedata/image/201902/24/2019022401106_0.jpg'
+  },
+  {
+    name : 'kimchi',
+    image : 'https://www.bgw.kr/wp-content/uploads/2019/12/%EC%88%98%EC%9E%85%EA%B9%80%EC%B9%98.png'
+  }
+];
+
 function App() {
-  return (
-    <div>
-      <h1>Hello</h1>
-    </div>
-  );
+  return ( 
+  <div>
+  { foodILike.map(dish => (
+    <Food name={dish.name} picture={dish.image}/>
+      ) 
+    ) }
+  </div>
+  )
 }
 
 export default App;
