cedd38ab1d3f9d782fe9ffbab02b5e052ef8a084
김동현 <dobedh@gmail.com>
2020-07-23 23:27:35 +0900

    200723_reacti_study

--- src/App.js
+++ src/App.js
@@ -1,38 +1,51 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
-function Food({ name, picture }){
-return ( 
-  <div>
-    <h2>I like { name }</h2>
-    <img src={picture} />
-  </div>
-)
-  }
+function Food({ name, picture, rating }){
+  return ( 
+    <div>
+      <h2>I like { name }</h2>
+      <h2>{rating}/5.0</h2>
+      <img src={picture} alt={name}/>
+    </div>
+    )
+}
 
 const foodILike = [
   {
+    id : 1,
     name : 'pizza',
-    image : 'https://www.recipetineats.com/wp-content/uploads/2020/05/Pepperoni-Pizza_7.jpg'
+    image : 'https://www.recipetineats.com/wp-content/uploads/2020/05/Pepperoni-Pizza_7.jpg',
+    rating : 5
   },
   {
+    id : 2,
     name : 'samgyeopsal',
-    image : 'https://image.chosun.com/sitedata/image/201902/24/2019022401106_0.jpg'
+    image : 'https://image.chosun.com/sitedata/image/201902/24/2019022401106_0.jpg',
+    rating : 4.5
   },
   {
+    id : 3,
     name : 'kimchi',
-    image : 'https://www.bgw.kr/wp-content/uploads/2019/12/%EC%88%98%EC%9E%85%EA%B9%80%EC%B9%98.png'
+    image : 'https://www.bgw.kr/wp-content/uploads/2019/12/%EC%88%98%EC%9E%85%EA%B9%80%EC%B9%98.png',
+    rating : 3
   }
 ];
 
 function App() {
   return ( 
   <div>
-  { foodILike.map(dish => (
-    <Food name={dish.name} picture={dish.image}/>
-      ) 
-    ) }
+  {foodILike.map(dish => (
+  <Food key={dish.id} name={dish.name} picture={dish.image} rating={dish.rating}/>
+  ))}
   </div>
   )
 }
+
+Food.propTypes = {
+  name : PropTypes.string.isRequired,
+  picture : PropTypes.string.isRequired,
+  rating : PropTypes.string.isRequired,
+};
 
 export default App;
